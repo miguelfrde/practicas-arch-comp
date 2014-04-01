d92b79917ac853cb13c305ca08838358e167d64e
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-04-01 18:37:04 -0500

    Update DataMemory and add data file to init memory

--- Practica2/MIPSModelsim/DataMemory.v
+++ Practica2/MIPSModelsim/DataMemory.v
@@ -1,47 +1,28 @@
-/******************************************************************
-* Description
-*	This is  a RAM memory that represents the data memory. 
-*  The initial values of this memory are written from a file named data.dat.
-* Version:
-*	1.0
-* Author:
-*	MSc. José Luis Pizano Escalante
-* Date:
-*	01/03/2014
-******************************************************************/
-module DataMemory
-#(parameter DATA_WIDTH=32
+
+
+module DataMemory 
+#(	parameter DATA_WIDTH=8,
+	parameter MEMORY_DEPTH = 1024
+
 )
 (
-	input [(DATA_WIDTH-1):0] WriteData,
-	input [(DATA_WIDTH-1):0] Address,
+	input [DATA_WIDTH-1:0] WriteData,
+	input [DATA_WIDTH-1:0]  Address,
 	input MemWrite,MemRead, clk,
-	output [(DATA_WIDTH-1):0] ReadData
+	output  [DATA_WIDTH-1:0]  ReadData
 );
-
-	// Declare the RAM variable
-	reg [DATA_WIDTH-1:0] ram[1023:0];
-
-	// Variable to hold the registered read address
-	reg [DATA_WIDTH-1:0] addr_reg;
-	reg [(DATA_WIDTH-1):0] ReadData_reg;
-
-	initial
-	begin
-		$readmemh("data.dat", ram);
-	end
 	
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[MEMORY_DEPTH-1:0];
+	wire [DATA_WIDTH-1:0] ReadDataAux;
+
 	always @ (posedge clk)
 	begin
 		// Write
 		if (MemWrite)
 			ram[Address] <= WriteData;
-			ReadData_reg <= ram[Address];
 	end
-
-
-		// read
-assign ReadData = {DATA_WIDTH{MemRead}} & ReadData_reg;
-			
+	assign ReadDataAux = ram[Address];
+  	assign ReadData = {DATA_WIDTH{MemRead}}& ReadDataAux;
 
 endmodule
