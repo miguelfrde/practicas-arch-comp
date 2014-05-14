4c463909068f0ca276f1e33779c4969cfa8fb830
IS682117 <IS682117@iteso.mx>
2014-05-14 18:53:32 -0500

    Working pipeline program

--- Practica3/program_pipeline.asm
+++ Practica3/program_pipeline.asm
@@ -18,160 +18,179 @@
 	addi $t0, $zero, 1
 	nop
 	nop
+	nop
 	sw $t0, 0($s0)
 	addi $t0, $zero, -3
+	nop
 	nop
 	nop
 	sw $t0, 4($s0)
 	addi $t0, $zero, 5
 	nop
 	nop
+	nop
 	sw $t0, 8($s0)
 	addi $t0, $zero, 7
+        nop
         nop
         nop
 	sw $t0, 12($s0)
 	addi $t0, $zero, 9
         nop
         nop
+        nop
 	sw $t0, 16($s0)
 	addi $t0, $zero, 0
+        nop
         nop
         nop
 	sw $t0, 20($s0)
 	addi $t0, $zero, 2
         nop
         nop
+        nop
 	sw $t0, 24($s0)
 	addi $t0, $zero, -4
+        nop
         nop
         nop
 	sw $t0, 28($s0)
 	addi $t0, $zero, 6
         nop
         nop
+        nop
 	sw $t0, 32($s0)
 	addi $t0, $zero, 8
+        nop
         nop
         nop
 	sw $t0, 36($s0)
 	addi $t0, $zero, 1
         nop
         nop
+        nop
 	sw $t0, 40($s0)
 	addi $t0, $zero, 3
+        nop
         nop
         nop
 	sw $t0, 44($s0)
 	addi $t0, $zero, -1
         nop
         nop
+        nop
 	sw $t0, 48($s0)
 	addi $t0, $zero, 0
+        nop
         nop
         nop
 	sw $t0, 52($s0)
 	addi $t0, $zero, 1
         nop
         nop
+        nop
 	sw $t0, 56($s0)
 	addi $t0, $zero, 0
+        nop
         nop
         nop
 	sw $t0, 60($s0)
 	addi $t0, $zero, 0
         nop
         nop
+	nop
 	sw $t0, 64($s0)
 	addi $t0, $zero, 0
+        nop
         nop
         nop
 	sw $t0, 68($s0)
 	addi $t0, $zero, -5
         nop
         nop
+	nop
 	sw $t0, 72($s0)
 	addi $t0, $zero, 7
         nop
         nop
+	nop
 	sw $t0, 76($s0)
 	addi $t0, $zero, 9
+        nop
         nop
         nop
 	sw $t0, 80($s0)
 	addi $t0, $zero, 0
         nop
         nop
+        nop
 	sw $t0, 84($s0)
 	addi $t0, $zero, 1
         nop
         nop
+	nop
 	sw $t0, 88($s0)
 	addi $t0, $zero, 0
         nop
         nop
-	sw $t0, 92($s0)
+        nop
+	sw $t0, 92($s0)	
 	
 	add $t0, $zero, $zero        # i = 0
+	nop
+	nop
+	nop	
 for_i: 
-	nop
-	nop
 	beq $t0, $s2, end_for_i      # until i == N
 	nop
-	add $t1, $zero, $zero        #   j = 0
+	add $t1, $zero, $zero        #   j = 0	
+	nop
+	nop
+	nop
 for_j:
-	nop
-	nop
 	beq $t1, $s3, end_for_j      #   until j == M
         nop
         add $a0, $zero, $t0          #     t2 = access_A(i, j) => 6i + 8j
-        add $a1, $zero, $t1          
+        add $a1, $zero, $t1    
         jal access_A
         nop
         nop
-        nop
         add $t2, $s0, $v0            #      A[i][j]
-        add $a0, $zero, $t1          #     t3 = access_B(j, i) => 8i + 8j
+        add $a0, $zero, $t1          #     t3 = access_B(j, i) => 8i + 8j        
         add $a1, $zero, $t0
         jal access_B
-        nop
         nop
         nop
         add $t3, $s1, $v0            #      B[j][i]
         lw $t4, 0($t2)		     #     load A[i][j][0]
         nop
         nop
+        nop
         sw $t4, 0($t3)               #     B[j][i][0] = A[i][j][0]
-        nop
-        nop
         lw $t4, 4($t2)               #     load A[i][j][1]
+        nop
         nop
         nop
         sub $t4, $zero, $t4          #     compute -A[i][j][1]
         nop
         nop
+        nop
         sw $t4, 4($t3)               #     B[j][i][0] = -A[i][j][0]
         addi $t1, $t1, 1             #     j++
+        nop
+        nop
         j for_j
-        nop
-        nop
         nop
 end_for_j:
 	addi $t0, $t0, 1             #   i++
-	nop	#
-	nop	#
 	j for_i
-	nop
-	nop
 	nop
 end_for_i:
 	j halt   
 	nop
-	nop 
-	nop    
         
-access_A:                            # Computes 24*i + 8*j with sums 
-	add $t4, $zero, $a0          # Computes 24*i
+access_A:                         # Computes 24*i + 8*j with sums 
+	add $t4, $zero, $a0       # Computes 24*i
 	add $t4, $t4, $a0
 	add $t4, $t4, $a0
 	add $t4, $t4, $a0
@@ -205,7 +224,6 @@ access_A:                            # Computes 24*i + 8*j with sums
 	add $t5, $t5, $a1
 	add $v0, $t4, $t5            # Return 24*i + 8*j
 	jr $ra
-	nop
 	nop
 	nop
          
@@ -252,7 +270,6 @@ access_B:                            # Computes 32*i + 8*j with sums
 	add $t5, $t5, $a1
 	add $v0, $t4, $t5            # Return 32*i + 8*j
 	jr $ra
-	nop
 	nop
 	nop
 halt:
